1876a78bd66a0556cc6d93495721df899f3a8025
Denis Bonci <matromix@gmail.com>
2017-06-24 16:12:14 +0200

    Add Artists controller

---
+++ src/AppBundle/Controller/ArtistsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View as RestView;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\CoverArtist;
+
+class ArtistsController extends FOSRestController
+{
+    /**
+     * @Rest\Get("/artists")
+     * @View(serializerGroups={"artists"})
+     */
+    public function getAllAction()
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:CoverArtist');
+        $artists = $repository->findAll();
+
+        return $artists;
+    }
+
+     /**
+      * @Rest\Post("/artists")
+      * @View(serializerGroups={"artists"})
+      */
+     public function postAction(Request $request)
+     {
+         $artist = new CoverArtist();
+         $artist->setName($request->get('name'));
+
+         $manager = $this->getDoctrine()->getManager();
+         $manager->persist($entity);
+         try {
+             $manager->flush();
+         } catch (Exception $e) {
+             return new RestView('Error', Response::HTTP_INTERNAL_SERVER_ERROR);
+         }
+
+         return new RestView($artist, Response::HTTP_OK);
+     }
+}
