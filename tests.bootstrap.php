2dbc9ebc02438d01b9c8c58ea56b6fbb3d0eb547
Denis Bonci <matromix@gmail.com>
2017-07-02 10:27:15 +0200

    Improve phpunit bootstrap file

--- tests.bootstrap.php
+++ tests.bootstrap.php
@@ -1,7 +1,7 @@
 <?php
-passthru('php bin/console doctrine:database:drop --force --env=test');
-passthru('php bin/console doctrine:database:create --env=test');
-passthru('php bin/console doctrine:schema:create --env=test');
-passthru('php bin/console doctrine:fixtures:load --purge-with-truncate --no-interaction --env=test');
+passthru('php bin/console doctrine:database:drop --quiet --force --if-exists --env=test');
+passthru('php bin/console doctrine:database:create --quiet --env=test');
+passthru('php bin/console doctrine:schema:create --quiet --no-interaction --env=test');
+passthru('php bin/console doctrine:fixtures:load --quiet --purge-with-truncate --no-interaction --env=test');
 
 require __DIR__.'/vendor/autoload.php';
